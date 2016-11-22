686eb287717ea2deb4868ba713b2030761e4d4f9
Michael Andersen <m.andersen@cs.berkeley.edu>
2016-11-21 18:51:02 -0800

    Add new ADC

--- cpu/samd21/periph/adc.c
+++ cpu/samd21/periph/adc.c
@@ -13,11 +13,11 @@
  * @file
  * @brief       ADC driver implementation
  *
+ * @author      Michael Andersen <m.andersen@berkeley.edu>
+ * @author      Hyun Sin Kim <hs.kim@berkeley.edu>
  * @author      Rane Balslev (SAMR21) <ranebalslev@gmail.com>
  * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
  * @author      Mark Solters <msolters@driblet.io>
- * @author      Michael Andersen <m.andersen@berkeley.edu>
- * @author      Hyun Sin Kim <hs.kim@berkeley.edu>
  *
  * @}
  */
@@ -32,130 +32,147 @@
 /* guard in case that no ADC device is defined */
 #if ADC_NUMOF
 
-int adc_init(adc_t dev) {
-#if ADC_0_EN
-  	while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+int adc_init(adc_t channel) {
 
-    /* Disable ADC Module before init. */
-    ADC_0_DEV->CTRLA.bit.ENABLE = 0;
+  	while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+
+    /*  Disable ADC Module before init. */
+    ADC_DEV->CTRLA.bit.ENABLE = 0;
 
   	/* Setup generic clock mask for adc */
   	PM->APBCMASK.reg |= PM_APBCMASK_ADC;
 
     /* GCLK Setup*/
-  	GCLK->CLKCTRL.reg = (uint32_t)(GCLK_CLKCTRL_CLKEN     |
-                                     GCLK_CLKCTRL_GEN_GCLK0 |
-                                     (ADC_GCLK_ID << GCLK_CLKCTRL_ID_Pos));
+    GCLK->CLKCTRL.reg = (uint32_t)(GCLK_CLKCTRL_CLKEN     |
+                                   GCLK_CLKCTRL_GEN_GCLK0 |
+                                   (ADC_GCLK_ID << GCLK_CLKCTRL_ID_Pos));
 
     /* Set RUN_IN_STANDBY */
-    ADC_0_DEV->CTRLA.bit.RUNSTDBY = 0;
+    ADC_DEV->CTRLA.bit.RUNSTDBY = 0;
 
     /* Set Voltage Reference */
-    ADC_0_DEV->REFCTRL.bit.REFSEL  = ADC_REFCTRL_REFSEL_INT1V_Val;
-    ADC_0_DEV->REFCTRL.bit.REFCOMP = 1;
+    ADC_DEV->REFCTRL.bit.REFSEL  = ADC_REFCTRL_REFSEL_INT1V_Val;
+    ADC_DEV->REFCTRL.bit.REFCOMP = 1;
 
     /* Set the accumulation and divide result */
-    ADC_0_DEV->AVGCTRL.bit.SAMPLENUM = 0;
-	  ADC_0_DEV->AVGCTRL.bit.ADJRES    = 0;
+    ADC_DEV->AVGCTRL.bit.SAMPLENUM = 0;
+	  ADC_DEV->AVGCTRL.bit.ADJRES    = 0;//ADC_AVGCTRL_ADJRES(divideResult) | accumulate;
 
     /* Set Sample length */
-    ADC_0_DEV->SAMPCTRL.bit.SAMPLEN = 0;
-	  while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+    ADC_DEV->SAMPCTRL.bit.SAMPLEN = 0;//ADC_SAMPCTRL_SAMPLEN(ADC_0_SAMPLE_LENGTH);
+	  while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
 
-    /* Configure CTRLB Register */
-    ADC_0_DEV->CTRLB.bit.DIFFMODE  = 0;
-  	ADC_0_DEV->CTRLB.bit.FREERUN   = 0;
-  	ADC_0_DEV->CTRLB.bit.CORREN    = 0;
-  	ADC_0_DEV->CTRLB.bit.LEFTADJ   = 1; // Left-adjusted results
-  	ADC_0_DEV->CTRLB.bit.RESSEL    = ADC_CTRLB_RESSEL_12BIT_Val;
-  	ADC_0_DEV->CTRLB.bit.PRESCALER = ADC_CTRLB_PRESCALER_DIV4_Val;
-  	while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+    /* Configure CTRLB Register HERE IS THE RESOLUTION SET!*/
+    ADC_DEV->CTRLB.bit.DIFFMODE  = 0;
+    ADC_DEV->CTRLB.bit.FREERUN   = 0;
+    ADC_DEV->CTRLB.bit.CORREN    = 0;
+    ADC_DEV->CTRLB.bit.LEFTADJ   = 1; // Left-adjusted results
+    ADC_DEV->CTRLB.bit.RESSEL    = ADC_CTRLB_RESSEL_12BIT_Val;
+    ADC_DEV->CTRLB.bit.PRESCALER = ADC_CTRLB_PRESCALER_DIV4_Val;
+    while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
 
-  	ADC_0_DEV->INPUTCTRL.bit.GAIN        = ADC_INPUTCTRL_GAIN_1X_Val;
-  	ADC_0_DEV->INPUTCTRL.bit.INPUTOFFSET = 0;
-  	ADC_0_DEV->INPUTCTRL.bit.MUXPOS      = ADC_INPUTCTRL_MUXPOS_PIN16_Val; // PA08 PIN (light sensor of hamilton)
-  	ADC_0_DEV->INPUTCTRL.bit.MUXNEG      = ADC_INPUTCTRL_MUXNEG_IOGND_Val;
-  	while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+    ADC_DEV->INPUTCTRL.bit.GAIN        = ADC_INPUTCTRL_GAIN_1X_Val;
+    ADC_DEV->INPUTCTRL.bit.INPUTOFFSET = 0;
+
+    /* Port configuration */
+    PM->APBCMASK.reg |= PM_APBBMASK_PORT;
+
+    int pin = ADC_GET_PIN(channel);
+    PortGroup* pg = ADC_GET_PORT_GROUP(channel);
+    int chan = ADC_GET_CHANNEL(channel);
+    
+    pg->DIRCLR.reg = (1 << pin);
+    pg->PINCFG[pin].bit.INEN = 1;
+    pg->PINCFG[pin].bit.PMUXEN = 1;
+    pg->PMUX[ADC_0_PIN >> 1].reg &= ~(0xf << (4 * (pin & 0x1)));
+  	pg->PMUX[ADC_0_PIN >> 1].reg |= (PORT_PMUX_PMUXE_B_Val << (4 * (pin & 0x1)));
+
+	  ADC_DEV->INPUTCTRL.bit.MUXNEG      = ADC_INPUTCTRL_MUXNEG_IOGND_Val;
+	  while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
 
     /* Configure Window Mode Register */
-    ADC_0_DEV->WINCTRL.reg = ADC_WINCTRL_WINMODE_DISABLE;
-    while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+    ADC_DEV->WINCTRL.reg = ADC_WINCTRL_WINMODE_DISABLE;
+    while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
 
     /* Enable interrupts and events related to result */
-    ADC_0_DEV->EVCTRL.bit.RESRDYEO = 1;
-    ADC_0_DEV->INTENSET.bit.RESRDY = 1;
+    ADC_DEV->EVCTRL.bit.RESRDYEO = 1;
+    //ADC_0_DEV->INTENCLR.bit.RESRDY = 1;
+    ADC_DEV->INTENSET.bit.RESRDY = 1;
 
-    /* ADC runs during debug mode */
-    ADC_0_DEV->DBGCTRL.bit.DBGRUN = 1;
+	  /* ADC runs during debug mode */
+    ADC_DEV->DBGCTRL.bit.DBGRUN = 1;
 
-    ADC_0_DEV->CTRLA.bit.SWRST = 1;
-    while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
-
-    //  Enable ADC Module.
-    ADC_0_DEV->CTRLA.bit.ENABLE = 1;
-    while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+    ADC_DEV->CTRLA.bit.SWRST = 1;
+    while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
 
     return 0;
-#endif
 }
 
 
 
-int adc_sample(adc_t dev, adc_res_t res){
+int adc_sample(adc_t channel, adc_res_t res){
+	int output;
+
+	// Set input channel for ADC
+	switch(channel) {
 #if ADC_0_EN
-    int output;
-
-    /*  Enable bandgap */
-    SYSCTRL->VREF.reg |= SYSCTRL_VREF_BGOUTEN;
-
-    ADC_0_DEV->CTRLA.bit.SWRST = 1;
-
-    while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
-
-    ADC_0_DEV->CTRLA.bit.ENABLE = 1;
-
-    while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
-
-  	switch (res) {
-		case ADC_RES_8BIT:
-  			ADC_0_DEV->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_8BIT_Val;
-  			break;
-		case ADC_RES_10BIT:
-  			ADC_0_DEV->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_10BIT_Val;
-  			break;
-		case ADC_RES_12BIT:
-  			ADC_0_DEV->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_12BIT_Val;
-  			break;
-		case ADC_RES_16BIT:
-  			ADC_0_DEV->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_16BIT_Val;
-  			break;
-		default:
-  			return -1;
-  			break;
-  	}
-
-  	/* Wait for sync. */
-  	while (ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
-
-  	/* Start the conversion. */
-  	ADC_0_DEV->SWTRIG.reg = ADC_SWTRIG_START;
-
-  	/* Wait for the result. */
-  	while (!(ADC_0_DEV->INTFLAG.reg & ADC_INTFLAG_RESRDY));
-
-  	/* Read result */
-  	output = (int)ADC_0_DEV->RESULT.reg;
-  	while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
-
-    ADC_0_DEV->CTRLA.bit.ENABLE = 0;
-  	while(ADC_0_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
-
-  	/*  Disable bandgap */
-  	SYSCTRL->VREF.reg &= ~SYSCTRL_VREF_BGOUTEN;
-
-  	/* Return result. */
-  	return output;
+		case ADC_0_INPUT_CHANNEL:
+			ADC_DEV->INPUTCTRL.bit.MUXPOS = channel;
+			break;
 #endif
+		default:
+			return -1;
+	}
+
+	//  Enable bandgap
+	SYSCTRL->VREF.reg |= SYSCTRL_VREF_BGOUTEN;
+
+	// Set resolution
+	switch (res) {
+		case ADC_RES_8BIT:
+			ADC_DEV->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_8BIT_Val;
+			break;
+		case ADC_RES_10BIT:
+			ADC_DEV->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_10BIT_Val;
+			break;
+		case ADC_RES_12BIT:
+			ADC_DEV->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_12BIT_Val;
+			break;
+		case ADC_RES_16BIT:
+			ADC_DEV->CTRLB.bit.RESSEL = ADC_CTRLB_RESSEL_16BIT_Val;
+			break;
+		default:
+			return -1;
+			break;
+	}
+
+	// Wait for sync.
+	while (ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+
+	//  Enable ADC Module.
+	ADC_DEV->CTRLA.bit.SWRST = 1;
+	while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+    ADC_DEV->CTRLA.bit.ENABLE = 1;
+	while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+
+	// Start the conversion.
+	ADC_DEV->SWTRIG.reg = ADC_SWTRIG_START;
+
+	// Wait for the result.
+	while (!(ADC_DEV->INTFLAG.reg & ADC_INTFLAG_RESRDY));
+
+	// Read result
+	output = (int)ADC_DEV->RESULT.reg;
+	while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+
+    ADC_DEV->CTRLA.bit.ENABLE = 0;
+	while(ADC_DEV->STATUS.reg & ADC_STATUS_SYNCBUSY);
+
+	//  Disable bandgap
+	SYSCTRL->VREF.reg &= ~SYSCTRL_VREF_BGOUTEN;
+
+	// Return result.
+	return output;
 }
 
 
