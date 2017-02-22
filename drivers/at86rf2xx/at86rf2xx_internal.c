ed99150fb0c8e70e17278d633b208993d67b5d52
Hyungsin <hskim@berkeley.edu>
2017-02-21 16:23:32 -0800

    dutymac

--- drivers/at86rf2xx/at86rf2xx_internal.c
+++ drivers/at86rf2xx/at86rf2xx_internal.c
@@ -160,8 +160,7 @@ void at86rf2xx_configure_phy(at86rf2xx_t *dev)
     do {
         state = at86rf2xx_get_status(dev);
     }
-    while ((state == AT86RF2XX_STATE_BUSY_TX_ARET) || (state == AT86RF2XX_STATE_BUSY_RX_AACK) ||
-		   (state == AT86RF2XX_STATE_BUSY_TX)      || (state == AT86RF2XX_STATE_BUSY_RX) );
+    while ((state == AT86RF2XX_STATE_BUSY_TX_ARET) || (state == AT86RF2XX_STATE_BUSY_RX_AACK));
 
     /* we must be in TRX_OFF before changing the PHY configuration */
     at86rf2xx_force_trx_off(dev);
