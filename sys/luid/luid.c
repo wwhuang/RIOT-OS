eb93b539d1a7bfc045a13b2e9975c0ebeebb6cf5
Hyungsin <hskim@berkeley.edu>
2017-07-04 00:48:36 -0700

    hamilton board support

--- sys/luid/luid.c
+++ sys/luid/luid.c
@@ -23,10 +23,11 @@
 
 #include "assert.h"
 #include "periph/cpuid.h"
+#include "board.h"
 
 #include "luid.h"
 
-static uint8_t lastused = 1;
+static uint8_t lastused = 0;
 
 void luid_get(void *buf, size_t len)
 {
@@ -44,19 +45,27 @@ void luid_custom(void *buf, size_t len, int gen)
     }
 }
 
+#ifndef HAS_FACTORY_BLOCK
+#define HAS_FACTORY_BLOCK 0
+#endif
+
 void luid_base(void *buf, size_t len)
 {
     assert(buf && (len > 0));
 
     memset(buf, LUID_BACKUP_SEED, len);
 
+    if (HAS_FACTORY_BLOCK) {
+        memcpy(buf, fb_eui64, 8);
+    } else {
 #if CPUID_LEN
-    uint8_t *out = (uint8_t *)buf;
-    uint8_t cid[CPUID_LEN];
+        uint8_t *out = (uint8_t *)buf;
+        uint8_t cid[CPUID_LEN];
 
-    cpuid_get(cid);
-    for (size_t i = 0; i < CPUID_LEN; i++) {
-        out[i % len] ^= cid[i];
-    }
+        cpuid_get(cid);
+        for (size_t i = 0; i < CPUID_LEN; i++) {
+            out[i % len] ^= cid[i];
+        }
 #endif
+    }
 }
