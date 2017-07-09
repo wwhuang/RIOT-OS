94c4d498aacdc4f5f7b63c8bf8d19717199c9fc7
Hyungsin <hskim@berkeley.edu>
2017-07-09 01:29:34 -0700

    add caching capability to hdc1000 to mitigate redundant sensing

--- drivers/hdc1000/hdc1000.c
+++ drivers/hdc1000/hdc1000.c
@@ -27,11 +27,26 @@
 #include "xtimer.h"
 #include "periph/i2c.h"
 #include "hdc1000.h"
+#include "xtimer.h"
 
 #define ENABLE_DEBUG    (0)
 #include "debug.h"
 
 #define I2C_SPEED                  I2C_SPEED_FAST
+
+#ifndef HDC1000_RENEW_INTERVAL
+#define HDC1000_RENEW_INTERVAL 5000000ul
+#endif
+
+int16_t temp_cached = 0;
+int16_t hum_cached = 0;
+
+void hdc1000_renew_timer_cb(void* arg) {
+    temp_cached = 0;
+    hum_cached = 0;
+}
+
+xtimer_t hdc1000_renew_timer = {.callback = hdc1000_renew_timer_cb};
 
 int hdc1000_init(hdc1000_t *dev, const hdc1000_params_t *params)
 {
@@ -111,13 +126,23 @@ int hdc1000_get_results(const hdc1000_t *dev, int16_t *temp, int16_t *hum)
 
     if (status == HDC1000_OK) {
         /* if all ok, we convert the values to their physical representation */
+        uint16_t traw = ((uint16_t)buf[0] << 8) | buf[1];
+        temp_cached = (int16_t)((((int32_t)traw * 16500) >> 16) - 4000);
+
+        uint16_t hraw = ((uint16_t)buf[2] << 8) | buf[3];
+        hum_cached  = (int16_t)(((int32_t)hraw * 10000) >> 16);
+
         if (temp) {
-            uint16_t traw = ((uint16_t)buf[0] << 8) | buf[1];
-            *temp = (int16_t)((((int32_t)traw * 16500) >> 16) - 4000);
+            *temp = temp_cached;
+            temp_cached = 0;
         }
         if (hum) {
-            uint16_t hraw = ((uint16_t)buf[2] << 8) | buf[3];
-            *hum  = (int16_t)(((int32_t)hraw * 10000) >> 16);
+            *hum = hum_cached;
+            hum_cached = 0;
+        }
+
+        if ((hum_cached != 0) || (temp_cached != 0)) {
+            xtimer_set(&hdc1000_renew_timer, HDC1000_RENEW_INTERVAL);
         }
     }
 
@@ -126,6 +151,19 @@ int hdc1000_get_results(const hdc1000_t *dev, int16_t *temp, int16_t *hum)
 
 int hdc1000_read(const hdc1000_t *dev, int16_t *temp, int16_t *hum)
 {
+    /* Return cached data to mitigate redundant sensing */
+    if ((temp == NULL) && (hum != NULL) && (hum_cached != 0)) {
+        *hum = hum_cached;
+        hum_cached = 0;
+        xtimer_remove(&hdc1000_renew_timer);
+        return HDC1000_OK;
+    } else if ((temp != NULL) && (hum == NULL) && (temp_cached != 0)) {
+        *temp = temp_cached;
+        temp_cached = 0;
+        xtimer_remove(&hdc1000_renew_timer);
+        return HDC1000_OK;
+    }
+
     if (hdc1000_trigger_conversion(dev) != HDC1000_OK) {
         return HDC1000_BUSERR;
     }
